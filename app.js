33fe3ade48c09cceb52b6e9ae2d38ef6c01444b1
kaio <kaiogonzaga2017@outlook.com>
2021-04-12 12:52:19 -0300

    Finalização do projeto com utilização de socket.io

--- app.js
+++ app.js
@@ -4,6 +4,50 @@ var app = require('./config/server')
 
 /* parametrizar a porta de escuta */
 
-app.listen(80, function(){
-    console.log('Servidor Online')
+var server = app.listen(80, function(){
+        console.log('Servidor Online')
+    })
+
+/* importando as configurações do websocket e passando a porta de escuta */
+
+var io = require('socket.io').listen(server) 
+
+app.set('io', io)
+
+/* criar a conexão por websocket */
+
+io.on('connection', function(socket){
+    console.log('usuário conectou')
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou')
+    })
+
+    socket.on('msgParaServidor', function(data){
+
+        /* dialogo */
+        socket.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+         )
+
+         socket.broadcast.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem})
+
+            /* participantes */
+            if(parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+
+                    socket.emit(
+                        'participantesParaCliente', 
+                        {apelido: data.apelido}
+                    )
+            
+                    socket.broadcast.emit(
+                        'participantesParaCliente', 
+                        {apelido: data.apelido})
+
+          }
+
+    })
 })
